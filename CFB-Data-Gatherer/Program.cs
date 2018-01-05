cda5730434376e4a67b0b9be7fb120cf29db10e5
Isaac Zais <isaaczais@hotmail.com>
2018-01-05 13:02:15 -0600

    Base program files

---
+++ CFB-Data-Gatherer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CFB_Data_Gatherer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
